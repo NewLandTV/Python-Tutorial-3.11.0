85e475ff650f326d32e6d4fdd2e6fb1585393d41
NewLandTV <86148752+NewLandTV@users.noreply.github.com>
2024-03-25 17:40:21 +0900

    Add Variable

---
+++ 2_Variable.py
@@ -0,0 +1,20 @@
+a = 4
+b = 12
+msg = "Hello Python"
+line = """A
+B J KKK JkhTV 0= -s
+ d"""
+
+print(a)
+print(b)
+print(msg)
+print(line)
+print(a + b)
+
+a = 2
+
+print(a)
+
+a = a / 1
+
+print(a)
